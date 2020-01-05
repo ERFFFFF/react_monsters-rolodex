2fe49391a2ac5142f639a1977cbfdafcba60d264
erfffff <dakevcraft@yahoo.fr>
2020-01-05 01:13:28 +0100

    Updates

---
+++ precache-manifest.a044ae8e8f2db19d2ce405f53101c5db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1bab58d3936bde41757690c15cb2292",
+    "url": "/react_monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ad8598491c0630155981",
+    "url": "/react_monsters-rolodex/static/css/main.e8601f24.chunk.css"
+  },
+  {
+    "revision": "5f64445c3b396716b777",
+    "url": "/react_monsters-rolodex/static/js/2.7e82c9cb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react_monsters-rolodex/static/js/2.7e82c9cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ad8598491c0630155981",
+    "url": "/react_monsters-rolodex/static/js/main.24385a01.chunk.js"
+  },
+  {
+    "revision": "55ac54afd0155689358b",
+    "url": "/react_monsters-rolodex/static/js/runtime-main.91515b3c.js"
+  }
+]);
